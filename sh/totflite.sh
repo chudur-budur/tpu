99c3752940e24fee208ebfa44cffaa5d4165d460
khaled <khaled@nod-labs.com>
2021-06-14 22:12:30 -0400

    testing with spinenet49 retinanet

--- sh/totflite.sh
+++ sh/totflite.sh
@@ -2,7 +2,7 @@
 
 EXPORT_DIR="../checkpoints/spine49retina-tfl"
 CHECKPOINT_PATH="../checkpoints/spine49retina/model.ckpt"
-CONFIG_FILE="../checkpoints/spine190retina/spinenet49_retinanet.yaml"
+CONFIG_FILE="../checkpoints/spine49retina/spinenet49_retinanet.yaml"
 PARAMS_OVERRIDE=""  # if any.
 BATCH_SIZE=1
 INPUT_TYPE="image_bytes"
